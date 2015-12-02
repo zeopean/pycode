460ba76452254b144a62d4da94dcf59911752884
zeopean <1412512785@qq.com>
2015-12-02 23:10:38 +0800

    添加python 编程书籍

---
+++ email/email.class.py
@@ -0,0 +1,29 @@
+#encoding=utf8
+# author zeopean
+# date 2015-12-2
+
+import smtplib
+from email.mime.text import MIMEText
+
+class emailHeadle:
+	Host 	= ''
+	User	= ''
+	Pwd		= ''
+	postfix	= 'email from zeopean@163.com'
+
+	def SendTextEmail():
+		pass
+
+
+	def SendHtmlEmail():
+		pass
+
+	def SendMultiparEmail():
+		pass
+
+
+
+
+
+
+
