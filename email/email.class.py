f2debb90bc39e3598c72556c590b4dda04f7160d
zeopean <1412512785@qq.com>
2015-12-03 23:17:37 +0800

    添加python 编程书籍 更新邮件发送类

--- email/email.class.py
+++ email/email.class.py
@@ -4,24 +4,77 @@
 
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart		#发送附件
 
 class emailHeadle:
-	Host 	= ''
-	User	= ''
+	'定义系统登录常量'
+	Host 	= 'smtp.163.com'
+	User	= '@163.com'
 	Pwd		= ''
 	postfix	= 'email from zeopean@163.com'
 
-	def SendTextEmail():
-		pass
+	# 发送text文本格式的邮件
+	def SendTextEmail(self,ToUserLists ,sub , content):
+		header  = "<"+ emailHeadle.User +">"
+		msg 	= MIMEText(content , _subtype='plain' , _charset='utf-8')
+		msg['Subject']	= sub
+		msg['From']		= header
+		msg['To']		= ';'.join(ToUserLists)
+
+		#发送邮件
+		self.send(header , ToUserLists , msg)
 
 
-	def SendHtmlEmail():
-		pass
+	# 发送html格式的邮件
+	def SendHtmlEmail(self , toUserLists , subject ,content):
+		header  = "<"+ emailHeadle.User +">"
+		msg 	= MIMEText(content ,_subtype='html' , _charset='utf-8')
+		msg['Subject']  = subject
+		msg['From']		= header
+		msg['To']		= ';'.join(toUserLists)
 
-	def SendMultiparEmail():
-		pass
+		#发送邮件
+		self.send(header , toUserLists , msg)
 
 
+
+	def SendMultiparEmail(self,toUserLists , subject,filename ):
+		#创建MUli对象
+		multi = MIMEMultipart()
+		header  = "<"+ emailHeadle.User +">"
+
+		#构造附件
+		attart = MIMEText(open('E:\\1.txt' , 'rb').read() ,'base64' ,'gb2312')
+		attart["Content-Type"] = 'application/octet-stream'
+		attart["Content-Disposition"] = 'attachment; filename="1.doc"'
+		multi.attach(attart)
+
+		#邮件头
+		multi['To'] 	= ';'.join(toUserLists)
+		multi['From']	= header
+		multi['Subject']= subject
+
+		#发送
+		self.send(multi['From'] , multi['To'] , multi)
+
+#发送公共方法
+	def send(self , header , ToUsers , msg):
+		try:
+			server = smtplib.SMTP()
+			server.connect(emailHeadle.Host)
+			server.login(emailHeadle.User , emailHeadle.Pwd)
+			server.sendmail( header , ToUsers , msg.as_string())
+			server.quit()
+			return True
+		except Exception , e:
+			print str(e)
+			return False
+
+email = emailHeadle()
+#email.SendTextEmail(['1412512785@qq.com'] ,'pycode email demo' , 'hello world!我是我的')
+#email.SendHtmlEmail(['1412512785@qq.com'] ,'pycode email demo' , '<h1><font color="red">hello</font> world!我是我的</h1>')
+
+#email.SendMultiparEmail(['1412512785@qq.com'] ,'pycode email multipart demo' , '1.txt')
 
 
 
